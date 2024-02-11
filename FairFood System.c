a7bb11c3da16972671a29b2586d8832a40d526cf
Syed Huzaifa Nazim <142250085+SyedHuzaifaNazim@users.noreply.github.com>
2024-02-11 22:46:30 +0500

    Update FairFood System.c

--- FairFood System.c
+++ FairFood System.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <string.h>
+#include<string.h>
 #include <stdlib.h>
 #include <time.h>
-#include <windows.h>
 
 // Define a structure to hold user information
 struct UserInfo {
@@ -15,6 +14,7 @@ struct UserInfo {
 
 void generateLicense(struct UserInfo *user, FILE *file)
 {
+	char type[10];
     // Set role prefix based on user's role
     switch (user->rolePrefix)
     {
@@ -33,7 +33,8 @@ void generateLicense(struct UserInfo *user, FILE *file)
         default:
             printf("Invalid role. Please enter 'f' for farmer, 'w' for wholesaler, or 'r' for retailer.\n");
             return;
-    }
+	}
+	
 
     // Get user information
     printf("Enter your NIC: ");
@@ -50,14 +51,30 @@ void generateLicense(struct UserInfo *user, FILE *file)
 
     // Generate a 6-digit license number
     user->licenseNumber = 100000 + rand() % 900000;
-
+    
     // Save user information and license number to the respective file
     fprintf(file, "%c%06d\t%s\t%s\t%s\n", user->rolePrefix, user->licenseNumber, user->nic, user->name, user->fatherName);
+    
+	    if (user->rolePrefix == 'F') {
+	    strcpy(type, "Farmer");
+		} else if (user->rolePrefix == 'W') {
+		    strcpy(type, "Wholesaler");
+		} else {
+	    strcpy(type, "Retailer");
+		}
 
     // Display the license number to the user
     system("cls");
-    
-    printf("Your license number: %c%06d\n", user->rolePrefix, user->licenseNumber);
+    printf("\n\n                   --------------------------------\n");
+	printf("\t  (((((((( Congratulation You Are Registered )))))))))\n");
+	printf("                   --------------------------------\n\n\n\n");
+    printf("\t _________________________________\n\n");
+    printf("\t| -Your NIC: %s\n",user->nic);
+    printf("\t| -Your Name: %s\n",user->name);
+    printf("\t| -Your Father Nmae: %s\n",user->fatherName);
+    printf("\t| -Aloted license number: %c%06d\n", user->rolePrefix, user->licenseNumber);
+    printf("\t| -Type: %s\n",type);
+    printf("\t _________________________________\n\n\n\n\n");
 }
 
 void fileComplaint()
@@ -65,20 +82,21 @@ void fileComplaint()
     char userID[20];
     char complaint[100];
     char licenseNumber[8];
-
-    printf("Enter your ID: ");
+    printf(" -Enter your ID: ");
     scanf("%s", userID);
 
-    printf("Describe your complaint: ");
+    printf(" -Describe your complaint: ");
     scanf(" %[^\n]", complaint);
 
-    printf("Enter the license number you are complaining about: ");
+    printf(" -Enter the license number you are complaining about: ");
     scanf("%s", licenseNumber);
 
     FILE *complaintFile = fopen("complaints.txt", "a");
     fprintf(complaintFile, "Complainant_ID: %s\nComplaint: %s\nLicense Number: %s\n\n", userID, complaint, licenseNumber);
     fclose(complaintFile);
-    printf("Complaint filed successfully.\n");
+    printf("\n\n                   --------------------------------\n");
+	printf("\t  (((((((( Complaint filed successfully. )))))))))\n");
+	printf("                   --------------------------------\n\n\n\n\n");
 }
 
 int countComplaints(const char *licenseNumber)
@@ -106,24 +124,29 @@ int countComplaints(const char *licenseNumber)
 
 int main()
 {
-	do{
+		
 	struct UserInfo user;
 	
-    char choice;
-
-    printf("\n\n                           Do you want to register (R) or complain (C) or you want to know the number of complaint of a license (N)? ");
-    scanf("%c\n\n", &choice);
-
+    char choice;;
+    system("color a");
+    printf("\n\n                   --------------------------------\n");
+	printf("\t  (((((((( Welcome to our Fair Food System )))))))))\n");
+	printf("                   --------------------------------\n");
+    printf("\n\n\n -Do you want to register press (R)\n -For complain press (C)\n -If you want to know the number of complaint of a license press (N)\n");
+    printf("\n -Your Option: ");
+    scanf(" %c", &choice);
     if (choice == 'R' || choice == 'r')
 	{
 		char role;
 	    FILE *file;
-	
-	    printf("Who are you? (Enter 'f' for farmer, 'w' for wholesaler, or 'r' for retailer): ");
+		system("cls");
+	    printf("\n\nWho are you? (Enter 'f' for farmer, 'w' for wholesaler, or 'r' for retailer): ");
 	    scanf(" %c", &role);
 	
 		user.rolePrefix = role;
-
+		system("cls");
+		system("color 7");
+		printf("\n\n");
 	    switch (role)
         {
             case 'f':
@@ -152,29 +175,28 @@ int main()
 	
 	else if (choice == 'C' || choice == 'c')
 	{
+		system("cls");
+		printf("\n\n");
        fileComplaint();
     }
 	
 	else if (choice == 'N' || choice == 'n')
 	{
+		system("cls");
 	    char licenseNumber[8];
-	    printf("Enter your license number to check complaints: ");
+	    printf("\n\n -Enter your license number to check complaints: ");
 	    scanf("%s", licenseNumber);
-	
+//	    printf("\n\n\n\n");
+		system("cls");
 	    int complaintCount = countComplaints(licenseNumber);
-	    printf("Number of complaints against license number %s: %d\n", licenseNumber, complaintCount);
+	    printf("\n\n\n\n\t  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	    printf("\n *******  Number of complaints against license number %s is %d *******\n", licenseNumber, complaintCount);
+	    printf("\n\t  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\n\n\n");
 		}	
 	else
 	{
         printf("Invalid choice. Please enter 'R' to register or 'C' to complain.\n");
     }
- }
-}
- while(choice==0);
- 
-	printf(".....moye moye");
- 
- printf("hello");
+
     return 0;
 }
-
